c9b113010fbf1e4c351d702d980f137fe0ab71c4
Marbelly <me0763032023@unab.edu.sv>
2024-09-09 07:59:45 -0600

    Agregar archivos de proyecto.

---
+++ Capa desconectada/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Capa_desconectada
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.gridNotipado = new System.Windows.Forms.DataGridView();
+            this.gridTipado = new System.Windows.Forms.DataGridView();
+            this.btnObtenerNoTipado = new System.Windows.Forms.Button();
+            this.btnObtenerTipado = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gridNotipado)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridTipado)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnObtenerNoTipado);
+            this.groupBox1.Controls.Add(this.gridNotipado);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(334, 379);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "DataSet no Tipado";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnObtenerTipado);
+            this.groupBox2.Controls.Add(this.gridTipado);
+            this.groupBox2.Location = new System.Drawing.Point(419, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(334, 379);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "DataSet Tipado";
+            // 
+            // gridNotipado
+            // 
+            this.gridNotipado.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridNotipado.Location = new System.Drawing.Point(18, 32);
+            this.gridNotipado.Name = "gridNotipado";
+            this.gridNotipado.Size = new System.Drawing.Size(295, 210);
+            this.gridNotipado.TabIndex = 0;
+            // 
+            // gridTipado
+            // 
+            this.gridTipado.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridTipado.Location = new System.Drawing.Point(20, 32);
+            this.gridTipado.Name = "gridTipado";
+            this.gridTipado.Size = new System.Drawing.Size(295, 210);
+            this.gridTipado.TabIndex = 1;
+            // 
+            // btnObtenerNoTipado
+            // 
+            this.btnObtenerNoTipado.Location = new System.Drawing.Point(105, 310);
+            this.btnObtenerNoTipado.Name = "btnObtenerNoTipado";
+            this.btnObtenerNoTipado.Size = new System.Drawing.Size(106, 23);
+            this.btnObtenerNoTipado.TabIndex = 1;
+            this.btnObtenerNoTipado.Text = "Obtener no Tipado";
+            this.btnObtenerNoTipado.UseVisualStyleBackColor = true;
+            this.btnObtenerNoTipado.Click += new System.EventHandler(this.btnObtenerNoTipado_Click);
+            // 
+            // btnObtenerTipado
+            // 
+            this.btnObtenerTipado.Location = new System.Drawing.Point(122, 310);
+            this.btnObtenerTipado.Name = "btnObtenerTipado";
+            this.btnObtenerTipado.Size = new System.Drawing.Size(98, 23);
+            this.btnObtenerTipado.TabIndex = 2;
+            this.btnObtenerTipado.Text = "Obtener Tipado";
+            this.btnObtenerTipado.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(768, 439);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.gridNotipado)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridTipado)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnObtenerNoTipado;
+        private System.Windows.Forms.DataGridView gridNotipado;
+        private System.Windows.Forms.Button btnObtenerTipado;
+        private System.Windows.Forms.DataGridView gridTipado;
+    }
+}
+
---
+++ Capa desconectada/Form1.cs
@@ -0,0 +1,29 @@
+﻿using AccesoDatos;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Capa_desconectada
+{
+    public partial class Form1 : Form
+    {
+        private CustomerRepository customerRepository = new CustomerRepository();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnObtenerNoTipado_Click(object sender, EventArgs e)
+        {
+            gridNotipado.DataSource = customerRepository.ObtenerTodos();
+        }
+    }
+}
